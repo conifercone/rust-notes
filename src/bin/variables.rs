3267003afa663ff9a202503cf0b38549f83bdd34
Kaiyu Shan <kaiyu.shan@outlook.com>
2025-08-27 17:00:36 +0800

    feat(variables): 忽略未使用的变量

--- src/bin/variables.rs
+++ src/bin/variables.rs
@@ -1,6 +1,9 @@
 fn main() {
+    // 变量解构和绑定
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    // 忽略未使用的变量
+    let _x = 5;
 }
